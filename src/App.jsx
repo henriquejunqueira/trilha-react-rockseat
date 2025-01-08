77dea00676e566d07b6dc14292ee3aca48316611
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2025-01-08 12:25:29 -0300

    Primeiro componente React

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
-  // throw new Error('Eita Giovana, o forninho caiu!');
-
-  return <h1>Hello ReactJS</h1>;
+  return <RepositoryList />;
 }
