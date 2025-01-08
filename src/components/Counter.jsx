a39d8aea4ba7e5ab96917ee8a58d0ecb7b982229
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2025-01-08 15:11:20 -0300

    Conceito de imutabilidade no react

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,15 @@
 import { useState } from 'react';
 
+// Imutabilidade
+
+// usuarios = ['diego3g', 'diegosf', 'danileao'];
+
+// usuarios.push('rafacamarda'); // ! dessa forma é acessada a memória para adicionar o novo valor
+
+// ? Utilizando o conceito de imutabilidade deve ser feito da seguinte forma utilizando spread operator:
+
+// novosUsuarios = [...usuarios, 'rafacamarda']; // ! o spread operator pega o que já existe e adiciona dados
+
 export function Counter() {
   const [counter, setCounter] = useState(0);
 
