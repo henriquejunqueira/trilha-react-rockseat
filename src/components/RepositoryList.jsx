0c30134b8cfcbf6cd2848757f4e91be9b4352998
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2025-01-08 16:17:23 -0300

    Utilizando o hook useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,6 @@
 import { RepositoryItem } from './RepositoryItem';
 import '../styles/repositories.scss';
+import { useState, useEffect } from 'react';
 
 const repository = {
   name: 'unform2',
@@ -8,6 +9,19 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  // a função dentro do useEffect vai ser executada sempre que 'repositories' tiver o valor alterado
+  // ! se a dependência do useEffect [] estiver vazia, a função é executada apenas uma vez
+  //   useEffect(() => {
+  //   }, [repositories]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/henriquejunqueira/repos')
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className='repository-list'>
       <h1>Lista de repositórios</h1>
